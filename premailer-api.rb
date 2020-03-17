0085792206021967880075a282844b7cafce3d72
Bienvenido David <biendavid@gmail.com>
2020-03-17 14:46:42 -0400

    Redis sentinels

--- premailer-api.rb
+++ premailer-api.rb
@@ -6,13 +6,22 @@ require 'redis'
 
 REDIS_HTML_EXPIRY = 600
 
-REDIS_HOST = ENV['REDIS_HOST']
-REDIS_PORT = ENV['REDIS_PORT']
+REDIS_SENTINELS = ENV['REDIS_SENTINELS']
 REDIS_DB = ENV['REDIS_DB']
 REDIS_PASSWORD = ENV['REDIS_PASSWORD']
-if REDIS_HOST.nil? || REDIS_PORT.nil? || REDIS_DB.nil? || REDIS_PASSWORD.nil?
-  puts "Environment variables REDIS_HOST, REDIS_PORT, REDIS_DB and REDIS_PASSWORD are required."
+REDIS_MASTER = ENV['REDIS_MASTER']
+
+if REDIS_SENTINELS.nil? || REDIS_DB.nil? || REDIS_PASSWORD.nil? || REDIS_MASTER.nil?
+  puts "Environment variables REDIS_SENTINELS, REDIS_DB, REDIS_PASSWORD and REDIS_MASTER are required."
   exit
+end
+
+Sentinel = Struct.new(:host, :port, :password)
+sentinels = Array.new 
+REDIS_SENTINELS.split(",").each do |host|
+    hostAndPort = host.split(":")
+    sentinel = Sentinel.new(hostAndPort[0], hostAndPort[1])
+    sentinels.push(sentinel)
 end
 
 configure do
@@ -34,7 +43,7 @@ post '/api/0.1/documents' do
   # Write the HTML output to Redis
   htmlFilename = "#{SecureRandom.uuid}.html"
   htmlContent = premailer.to_inline_css
-  redis = Redis.new(:host => REDIS_HOST, :port => REDIS_PORT, :db => REDIS_DB, :password => REDIS_PASSWORD)
+  redis = Redis.new(url: "redis://" + REDIS_MASTER, sentinels: sentinels, password: REDIS_PASSWORD, db: REDIS_DB)
   redis.setex(htmlFilename, REDIS_HTML_EXPIRY, htmlContent)
 
   htmlPath = "html/#{htmlFilename}"
@@ -43,7 +52,7 @@ post '/api/0.1/documents' do
 end
 
 get '/html/:filename' do |filename|
-  redis = Redis.new(:host => REDIS_HOST, :port => REDIS_PORT, :db => REDIS_DB, :password => REDIS_PASSWORD)
+  redis = Redis.new(url: "redis://" + REDIS_MASTER, sentinels: sentinels, password: REDIS_PASSWORD, db: REDIS_DB)  
   content = redis.get(filename)
 
   if content.nil?
